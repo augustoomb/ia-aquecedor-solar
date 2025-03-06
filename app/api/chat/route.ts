b591faf0bf3edbd248d6b8ad4d3b5c608084385f
Augusto Barbosa <augustobarbosa1988@gmail.com>
2025-03-06 11:37:10 -0300

    implementando google gemini api

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,19 @@
 import { NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
 
-export function GET(req: Request, res: NextApiResponse) {
-    return NextResponse.json({ message: 'Teste' }, { status: 200 });
+const geminiAPI = process.env.GEMINI_API_KEY;
+
+export async function GET(req: Request, res: NextApiResponse) {
+
+    const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+    const genAI = new GoogleGenerativeAI(geminiAPI);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const prompt = "Explain how AI works";
+
+    const result = await model.generateContent(prompt);
+    console.log(result.response.text());
+
+    return NextResponse.json({ message: result.response.text() }, { status: 200 });
 }   
