73752fe01e848b2d9588eec22c0f3b20f85ad298
Augusto Barbosa <augustobarbosa1988@gmail.com>
2025-03-07 18:48:46 -0300

    embeddings implementado

--- lib/langchain.ts
+++ lib/langchain.ts
@@ -1,25 +1,43 @@
 import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { GoogleGenerativeAIEmbeddings } from '@langchain/google-genai';
+import { TaskType } from "@google/generative-ai";
 
 export const teste = async () => {
-    const dataPath = "./data";
+    try {
+        const dataPath = "./data";
+        const geminiAPI = process.env.GEMINI_API_KEY;
 
-    /* Load all PDFs within the specified directory */
-    const directoryLoader = new DirectoryLoader(dataPath, {
-        ".pdf": (path: string) => new PDFLoader(path),
-    });
+        // LOADER
+        const directoryLoader = new DirectoryLoader(dataPath, {
+            ".pdf": (path: string) => new PDFLoader(path),
+        });
 
-    const directoryDocs = await directoryLoader.load();
+        const directoryDocs = await directoryLoader.load();
 
-    // console.log(directoryDocs[0]);
 
-    /* Additional steps : Split text into chunks with any TextSplitter. You can then use it as context or save it to memory afterwards. */
-    const textSplitter = new RecursiveCharacterTextSplitter({
-        chunkSize: 1000,
-        chunkOverlap: 200,
-    });
 
-    const splitDocs = await textSplitter.splitDocuments(directoryDocs);
-    console.log(splitDocs[0]);
+        // SPLITER
+        const textSplitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 1000,
+            chunkOverlap: 200,
+        });
+
+        const splitDocs = await textSplitter.splitDocuments(directoryDocs);
+
+
+
+        // EMBEDDINGS
+        const embeddings = new GoogleGenerativeAIEmbeddings({
+            apiKey: geminiAPI,
+            taskType: TaskType.RETRIEVAL_DOCUMENT,
+            modelName: "embedding-001", // Modelo de embedding gratuito do Google
+        });
+
+        console.log("chegou aqui sem erro")
+    } catch (error) {
+        console.log("ERRO!")
+    }
+
 }
